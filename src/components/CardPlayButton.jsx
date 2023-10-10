328ad5d6b23e682ae560e5047268fc21296e4819
pablogomez <pablo6189@gmail.com>
2023-10-10 12:32:19 -0300

    Zustand global state added

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,32 @@
+import { Pause, Play } from "./Player"
+import { usePlayerStore } from '@/store/playerStore'
+
+export  function CardPlayButton({ id }) {
+
+    const { 
+        currentMusic, 
+        isPlaying, 
+        setIsPlaying, 
+        setCurrentMusic, 
+    } = usePlayerStore(state => state)
+
+    const handleClick = () => {
+
+        setCurrentMusic({
+            playlist: {
+                id
+            }
+        })
+
+        setIsPlaying(!isPlaying)
+    }
+
+    const isPlayingPlayList = isPlaying && currentMusic?.playlist.id === id
+
+    return (
+        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
+            { isPlayingPlayList ? <Pause /> : <Play /> }
+        </button>
+    )
+}
+
